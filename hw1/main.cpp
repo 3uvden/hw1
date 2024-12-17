b04b4dba984e79420ca7a60f5777d6400aeb4084
panda <panda@DESKTOP-HI8KV0P>
2024-12-17 13:24:13 +0500

    Добавьте файлы проекта.

---
+++ hw1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() 
+{
+    
+    std::cout << "----------------------\n< Happy SysAdmin Day! >\n----------------------\n";
+    std::cout << "\t  \\   ^__^\n";
+    std::cout << "\t   \\  (oo)\\_______\n";
+    std::cout << "\t      (__)\\       )\\/\\ \n";
+    std::cout << "\t          ||----w |\n";
+    std::cout << "\t          ||     ||\n";
+
+    return 0;
+}
